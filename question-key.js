4eb09920e4e114504e40e40ceced4ce758acb790
Caleb Ford <kylecford@icloud.com>
2020-06-05 23:02:13 -0700

    changed name of array to choices

--- question-key.js
+++ question-key.js
@@ -1,112 +1,112 @@
 const STORE = [
     {
       question: 'Where is user Photos App content stored?',
-      answers: [
+      choices: [
         'Photos App',
         'Photos Library',
         'Photos Database',
         'Photos Directory'
       ],
-      correctAnswer:
+      answer:
         'Photos Library'
     },
     {
       question:
         'How long does content remain in recentley deleted?',
-      answers: [
+      choices: [
         '24 hours',
         '90 days',
         '30 days',
         '14 days'
       ],
-      correctAnswer:
+      answer:
         '30 days'
     },
     {
       question:
         'In addition to Live, how many Live Photo Effects are there?',
-      answers: [
+      choices: [
         '3',
         '4',
         '2',
         'None'
       ],
-      correctAnswer: '3'
+      answer: '3'
     },
     {
       question: 'How do you import to the Photos App?',
-      answers: [
+      choices: [
         'Drag content to within Photos App window',
         'Drag content to Photos App icon',
         'Go to File > Import, then navigate to location and choose review for import',
         'All of the above'
       ],
-      correctAnswer: 'All of the above'
+      answer: 'All of the above'
     },
     {
       question:
         'All except which are ways of sharing from the Photos App?',
-      answers: [
+      choices: [
         'In menu bar go to Share > ...',
         'Click the Share Button in the toolbar and make choice',
         'In menu bar go to File > Share > ...',
         'In menu bar go to File > Export > ...'
       ],
-      correctAnswer:
+      answer:
         'In menu bar go to Share > ...'
     },
     {
       question: 'Content in the Photos section of Library can be sorted by all of the following except?',
-      answers: [
+      choices: [
         'Years',
         'Months',
         'Days',
         'Accumulative'
       ],
-      correctAnswer: 'Accumulative'
+      answer: 'Accumulative'
     },
     {
       question:
         'How do you create a new Media Type in Albums?',
-      answers: [
+      choices: [
         'Go to File > Create > Media Type',
         'You can\'t. It is based off the type of content present',
         'Upon import, choose which type of Media your importing',
         'Secondary click a Picture or Video and choose "Change Media Type" to create category'
       ],
-      correctAnswer:
+      answer:
         'You can\'t. It is based off the type of content present'
     },
     {
       question: 'If you suddenly have 10,000 duplicate items, what is a likely cause?',
-      answers: [
+      choices: [
         '"Duplicate Items" was used after having highlighting all',
         'The Photos App has a bug which needs to be fixed',
         'The Pictures System Folder was likely mistakenly imported to the Photos App',
         'A 3rd party App is likely the cause'
       ],
-      correctAnswer:
+      answer:
         'The Pictures System Folder was likely mistakenly imported to the Photos App'
     },
     {
       question: 'How could you best address an accidental import?',
-      answers: [
+      choices: [
         'Go to the Imports area, highlight and delete affected import',
         'Download a duplicate cleaning App',
         'Go through all duplicate items and delete each item manually',
         'Live with issue, and get used to seeing double'
       ],
-      correctAnswer: 'Go to the Imports area, highlight and delete affected import'
+      answer: 'Go to the Imports area, highlight and delete affected import'
     },
     {
       question:
         'All of the following are ways to organize content in the Photos App except?',
-      answers: [
+      choices: [
         'Albums',
         'Smart Albums',
         'Folders',
         'Book'
       ],
-      correctAnswer: 'Book'
+      answer: 'Book'
     }
   ];
