d6acd6c0f16ffa12be5d594d4edf8b0eba13fa28
Caleb Ford <kylecford@icloud.com>
2020-06-06 11:16:33 -0700

    Corrected JavaScript to questionResult()

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function generateWelcomePage() {
                 <h2>macOS® Basics</h2>
                 <h3>Photos App</h3>
             </div>
-            <button class="get-started-button js-get-started-button">Get Started</button>
+            <button class="get-started-button js-get-started-button js-submit">Get Started</button>
         </div>`;
 }
 
@@ -26,23 +26,23 @@ function getStarted() {
 function generateQuestion() {
     let quizChef = $(`
         <form class="js-generated-quiz">
-            <fieldset>
+            <fieldset class="js-landing-zone">
                 <legend>${STORE[i].question}
                 </legend>
             </fieldset>
         </form>`)
 
     let cookIngredients = $(quizChef).find('fieldset');
-
+    console.log(cookIngredients, "before")
     STORE[i].choices.forEach(function (answerValue, answerIndex) {
         $(`
         <input type="radio" id="${answerIndex}" name="answer" value=${answerValue}" required>
         <label for="${answerIndex}">${answerValue}</label>
-
+        
       `).appendTo(cookIngredients);
     });
-
-    $(`<button type="submit" class="next-question" value="Submit">Submit</button>`).appendTo(cookIngredients);
+    console.log(cookIngredients, "after")
+    $(`<button type="submit" class="submit js-submit" value="Submit">Submit</button>`).appendTo(cookIngredients);
     $(`<ul class="entire-status-bar">
                 <li class="status-bar-third" id="right-wrong">Question:
                     <span class="questionNumber">${questionNumber}</span>/10</li>
@@ -69,14 +69,33 @@ function generateQuestionDisplay() {
 
 function quizMain() {
     console.log('`quizMain` ran')
-    $('.js-landing-zone').on('click', '.js-get-started-button', function(event) {
+    $('.js-landing-zone').on('click', '.js-submit', function(event) {
         const renderedQuestion = generateQuestionDisplay();
         $('.js-landing-zone').html(renderedQuestion);
     })
 }
 
+function correct() {
+    console.log('`correct` ran')
+    i += 1;
+    console.log(i)
+}
+
+function incorrect() {
+    console.log('`correct` ran')
+    let insert = $(`<br><span class="incorrect">That is incorrect, the correct answer is ${STORE[i].answer}</span>`)
+    alert(insert)
+}
+
 function questionResult() {
     console.log('`questionResult` ran')
+    $('.js-generated-quiz').on('click', '.js-submit', function() {
+        if ($('input:checked') === STORE[i].answer) {
+            correct();
+        } else {
+            incorrect();
+        }
+    });
 }
 
 function restart() {
